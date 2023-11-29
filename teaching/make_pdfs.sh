121f226a0dccbff603b3526a8fee60f37e062e1e
Flavio Hafner <f.hafner@esciencecenter.nl>
2023-11-29 13:35:30 +0100

    add pdf for linear models

--- teaching/make_pdfs.sh
+++ teaching/make_pdfs.sh
@@ -6,4 +6,7 @@ wkhtmltopdf temp.html predictive_modeling_pipeline.pdf
 jupyter nbconvert --execute --to html --output temp.html selecting_the_best_model.ipynb 
 wkhtmltopdf temp.html selecting_the_best_model.pdf 
 
+jupyter nbconvert --execute --to html --output temp.html linear_models.ipynb 
+wkhtmltopdf temp.html linear_models.pdf 
+
 rm -rf temp.html
