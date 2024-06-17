89c4c91d9b805924439f28cf73af09d877d8b784
Flavio Hafner <f.hafner@esciencecenter.nl>
2024-06-17 18:06:33 +0200

    update for sicss 2024

--- teaching/make_pdfs.sh
+++ teaching/make_pdfs.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-jupyter nbconvert --execute --to html --output temp.html predictive_modeling_pipeline.ipynb 
-wkhtmltopdf temp.html predictive_modeling_pipeline.pdf 
+# jupyter nbconvert --execute --to html --output temp.html predictive_modeling_pipeline.ipynb 
+# wkhtmltopdf temp.html predictive_modeling_pipeline.pdf 
 
-jupyter nbconvert --execute --to html --output temp.html selecting_the_best_model.ipynb 
-wkhtmltopdf temp.html selecting_the_best_model.pdf 
+# jupyter nbconvert --execute --to html --output temp.html selecting_the_best_model.ipynb 
+# wkhtmltopdf temp.html selecting_the_best_model.pdf 
 
-jupyter nbconvert --execute --to html --output temp.html linear_models.ipynb 
-wkhtmltopdf temp.html linear_models.pdf 
+# jupyter nbconvert --execute --to html --output temp.html linear_models.ipynb 
+# wkhtmltopdf temp.html linear_models.pdf 
+
+cd 2024-sicss
+jupyter nbconvert --execute --to html --output temp.html data-exploration.ipynb 
+wkhtmltopdf temp.html data-exploration.pdf 
+
+jupyter nbconvert --execute --to html --output temp.html categorical-encoding.ipynb 
+wkhtmltopdf temp.html categorical-encoding.pdf 
 
 rm -rf temp.html
